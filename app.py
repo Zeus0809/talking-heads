d8ca87447fcea70d641d79cac10310c1eafd1037
Zeus0809 <ikkit2002@gmail.com>
2025-02-12 15:26:03 -0500

    Looked into HTML&CSS styling of streamlit elements using st.markdown(). Injected CSS to remove default padding and center the title on page. Also set the layout to wide.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import requests
 
+TITLE = "Welcome to Talking Heads AI"
+
 API_URL_MODELS = "http://192.168.0.122:1234/v1/models"
 API_URL_CHAT = "http://192.168.0.122:1234/v1/chat/completions"
 model_name = ""
@@ -11,13 +13,29 @@ def get_llm_response(response):
     json = response.json()
     return json["choices"][0]["message"]["content"]
 
-# Get data from user
-user_model = input("What model would you like to ask? ").lower()
-if user_model == "deepseek" :
-    model_name = "deepseek-r1-distill-qwen-7b"
-else :
-    model_name = "mistral-7b-instruct-v0.3"
-prompt = input("Enter the initial prompt: ")
+def main():
+    st.set_page_config(layout="wide")
+    st.title(TITLE)
+    st.markdown("""
+        <style>
+        .block-container {
+            padding-top: 2rem;
+            padding-right: 0rem;
+            padding-left: 0rem;
+            padding-bottom: 0rem;
+            text-align: center
+        }
+        </style>
+    """,
+    unsafe_allow_html=True
+    )
+    col_left, col_center, col_right = st.columns([1, 2, 1], border=True)
+
+    
+
+
+
+
 
 # Construct the message for LLM server
 messages = [{"role" : "user", "content": prompt}]
@@ -29,15 +47,16 @@ payload = {
     "max_tokens" : 100
 }
 
+
 # API call to LLM
-response = requests.post(API_URL_CHAT, json=payload)
-
-if response.status_code == 200 :
-    print("AI: ", get_llm_response(response))
-else :
-    print("Error: ", response.status_code, response.text)
-
+# response = requests.post(API_URL_CHAT, json=payload)
+# if response.status_code == 200 :
+#     print("AI: ", get_llm_response(response))
+# else :
+#     print("Error: ", response.status_code, response.text)
 
 
 
 
+if __name__ == "__main__":
+    main()
