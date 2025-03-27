5d5e25ede1b85831cf534275131a9ac24a53b427
Zeus0809 <ikkit2002@gmail.com>
2025-03-27 13:03:28 -0400

    Added a max_turns control to select how many messages per conversation user wants to generate.

--- app.py
+++ app.py
@@ -56,14 +56,6 @@ def main():
 
     st.title(TITLE)
 
-    with st.sidebar:
-        # New conversation button
-        if st.button("New Conversation"):
-            st.session_state.talk_started = False
-            st.session_state.initial_prompt = ""
-            st.session_state.model_asked = ""
-            st.rerun()
-
     # start ollama
     ollama_tools.start_ollama()
     time.sleep(0.5) # wait for ollama to start
@@ -95,6 +87,19 @@ def main():
         st.session_state["left_system_prompt"] = ollama_tools.DEFAULT_SYSTEM_PROMPT_LEFT
     if "right_system_prompt" not in st.session_state:
         st.session_state["right_system_prompt"] = ollama_tools.DEFAULT_SYSTEM_PROMPT_RIGHT
+    if "max_turns" not in st.session_state:
+        st.session_state["max_turns"] = 6 # default to 6 messages per conversation
+
+    with st.sidebar:
+        st.title("Conversation Settings")
+        # New conversation button
+        if st.button("New Conversation"):
+            st.session_state.talk_started = False
+            st.session_state.initial_prompt = ""
+            st.session_state.model_asked = ""
+            st.rerun()
+        # Max turns slider
+        st.slider("Messages to generate per conversation", min_value=2, max_value=50, step=1, key="max_turns")
 
     # Header (3 tiles)
     ask_left, initial_propmt_box, ask_right = st.columns([1, 2, 1], border=False)
@@ -145,9 +150,8 @@ def main():
                 current_model_alias = st.session_state.model_asked
                 current_prompt = st.session_state.initial_prompt
                 current_system_prompt = st.session_state.left_system_prompt if current_model_alias == st.session_state.left_model_alias else st.session_state.right_system_prompt
-                max_turns = 8
 
-                for turn in range(max_turns):
+                for turn in range(st.session_state.max_turns):
                     # get the response from the current model and display it
                     current_model_name = get_model_name_by_alias(current_model_alias)
                     model_reply = ollama_tools.get_llm_response(current_model_name, current_system_prompt, message=current_prompt)
